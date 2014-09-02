bc7d4fc32c6bf8e2437003925e0de1dc53019e64
tbluteau <bluteau.thomas@gmail.com>
2014-09-02 09:38:58 +0200

    first git commit

---
+++ GitTest.Entities/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace GitTest.Entities
+{
+    public class BaseEntity
+    {
+        public string Id { get;set;}
+        public string Name { get; set; }
+
+        public Dictionary<string, string> ExtraData { get; set; }
+
+    }
+}
